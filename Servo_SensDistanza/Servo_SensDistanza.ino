8e84acaccece6e347a038c74631b6e45add9e735
Alberto Panu <alberto@panu.it>
2021-02-04 10:42:16 +0100

    wwwew

--- Servo_SensDistanza/Servo_SensDistanza.ino
+++ Servo_SensDistanza/Servo_SensDistanza.ino
@@ -1,28 +1,28 @@
 #include "SR04.h"
 #include <Servo.h>
-SR04 sr04 = SR04(11,12);
+SR04 sr04 = SR04(11, 12);
 
 int posServo = 0;
 Servo myservo;
 int distanza;
 void setup() {
   // put your setup code here, to run once:
-  pinMode(7,OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   myservo.attach(9);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  distanza=sr04.Distance();
-  if(distanza<10){
-    myservo.write(posServo++);
-    delay(100);
+  distanza = sr04.Distance();
+  if (distanza < 20) {
+    myservo.write(0);
+    //    delay(100);
     Serial.println(distanza);
-    digitalWrite(7,LOW);
+    digitalWrite(LED_BUILTIN, LOW);
   }
-  else{
-   myservo.write(posServo--);
-   digitalWrite(7,HIGH);
+  else {
+    myservo.write(180);
+    digitalWrite(LED_BUILTIN, HIGH);
   }
 }
